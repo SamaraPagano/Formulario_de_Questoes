6ac0d8aaf37ccec30bb7278d68b9088824206540
Samara de Oliveira Pagano <132520421+SamaraPagano@users.noreply.github.com>
2023-06-21 07:58:51 -0300

    Add files via upload

--- FormQuestionário.cs
+++ FormQuestionário.cs
@@ -14,8 +14,8 @@ namespace Trabalho
     public partial class FormQuestionário : Form
     {
         //Nome do arquivo onde as questões estão armazenadas
-        private string arquivoQuestoes = "questoes.txt";
-        private int tempoPorMateria = 20; //tempo em minutos por matéria
+        private string arquivoQuestoes = "Questões.txt";
+        private int tempoPorMateria = 5; //tempo em minutos por matéria
         private Timer timer;
         private int tempoRestante;
 
@@ -124,7 +124,7 @@ namespace Trabalho
         {
             tempoRestante = tempoPorMateria;
             timer = new Timer();
-            timer.Interval = 1000; //1 segundo
+            timer.Interval = 300; // segundo
             timer.Tick += Timer_Tick;
             timer.Start();
         }
@@ -143,5 +143,97 @@ namespace Trabalho
                 PararContagemTempo();
             }
         }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            VerificarResposta();
+        }
+        private void VerificarResposta()
+        {
+            string materia = cmbDisciplina.SelectedItem?.ToString();
+
+            if (string.IsNullOrWhiteSpace(materia))
+            return;
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(arquivoQuestoes))
+                {
+                    string line;
+                    bool encontrouQuestao = false;
+
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        if (line == materia)
+                        {
+
+                            //Encontrou uma questão da matéria selecionada
+                            encontrouQuestao = true;
+                            string pergunta = sr.ReadLine();
+                            sr.rdbA.Text = sr.ReadLine();
+                            sr.rdbB.Text = sr.ReadLine();
+                            sr.rdbC.Text = sr.ReadLine();
+                            sr.rdbD.Text = sr.ReadLine();
+                            sr.rdbE.Text = sr.ReadLine();
+
+                            //Verificar a resposta selecionada pelo usuário
+                            if (rdba.Checked && rdba.Text == respostaCorreta)
+                            {
+                                lblA.Text = "Resposta Correta";
+                            }
+                            else if (rdba.Checked && rdba.Text == respostaCorreta)
+                            {
+                                lblB.Text = "Resposta Correta";
+                            }
+                            else if (rdbC.Checked && rdbC.Text == respostaCorreta)
+                            {
+                                lblC.Text = "Resposta Correta";
+                            }
+                            else if (rdbD.Checked && rdbD.text == respostaCorreta)
+                            {
+                                lblD.Text = "Resposta Correta";
+                            }
+                            else if (rdbE.Checked && rdbE.Text == respostaCorreta)
+                            {
+                                lblE.Text = "Resposta Correta";
+                            }
+                        else
+                        {
+                            lblA.Text = "Resposta Incorreta";
+                        }
+                        else
+                        {
+                                lblB.Text = "Resposta Incorreta";
+                        }
+                        else 
+                        {
+                                lblC.Text = "Resposta Incorreta";
+                        }
+                        else
+                        {
+                                lblD.Text = "Resposta Incorreta";
+                        }
+                        else
+                        {
+                                lblE.Text = "Resposta Incorreta";
+                            }
+                            break;
+                        }
+                    }
+                    if (!encontrouQuestao)
+                    {
+                        MessageBox.Show("Não há mais questões disponíveis para a matéria selecionada", "Fim das questões", MessageBoxButtons.Ok, MessageBoxIcon.Information);
+                        LimparCampos();
+                        PararContagemTempo();
+                    }
+
+                }
+            } 
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocorreu um erro:" +ex.Message, "Erro", MessageBoxButtons.Ok, MessageBoxIcon.Erro);
+            }
+        }
+
     }
+    
 }
